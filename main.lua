f30d35a8cc86285a0e4fc2a95aa2ebe0aa32d08e
BaderSarah <bader.sarah@icloud.com>
2024-07-23 14:46:41 +0200

    modification

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 -- Mineturtle class
 
 local Mineturtle = {}
+Mineturtle.__index = Mineturtle
 
 -- constructor
 
-function Mineturtle:new(feul)
-    local instance = setmetatable({}, Mineturtle)
-    instance.feul = feul
+function Mineturtle:new(fuel)
+    local instance = setmetatable({}, self)
+    instance.fuel = fuel
     return instance
 end
 
@@ -16,24 +17,21 @@ function Mineturtle:get_block_type()
     local has_block, data = turtle.inspect()
 
     if has_block then
-        local information = textutils.serialise(data)
+        local information = textutils.serialize(data)
         print(information)
     end
-
 end
 
 -- main method
 
 function Mineturtle:dig()
-
     -- 1) see what block type it is
     -- 2) dig when block required
     -- 3) do this for up and down forward
-    -- // see shape of diging //
+    -- // see shape of digging //
     -- keep tracker to know how far you got to be able to return back
-    get_block_type()
+    self:get_block_type()
 end
 
 local mine_turtle = Mineturtle:new(turtle.getFuelLevel())
 mine_turtle:dig()
-
