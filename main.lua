b2d6167e695c340b76c63f37d0603b159cb8664b
BaderSarah <bader.sarah@icloud.com>
2024-07-30 14:38:56 +0200

    bug

--- main.lua
+++ main.lua
@@ -43,7 +43,7 @@ function Mineturtle:dig()
     -- 3) do this for up and down forward
     -- // see shape of digging //
     -- keep tracker to know how far you got to be able to return back
-    while(fuel > (fuel/2 + 3)) do -- no detect bcs water or lava
+    while(turtle.getFuelLevel() > ((turtle.getFuelLevel()/2) + 3)) do -- no detect bcs water or lava
         self:get_block_type()
         self:mine()
     end
